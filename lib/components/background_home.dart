b98d37a9609723d1353975827962544242ba00d0
rayanramazan <rayanramazan09@gmail.com>
2023-02-03 22:42:26 +0300

    add image and input search

---
+++ lib/components/background_home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BgHome extends StatelessWidget {
+  const BgHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.network(
+          'https://images.pexels.com/photos/6923390/pexels-photo-6923390.jpeg?auto=compress&cs=tinysrgb&w=1600',
+          height: 400,
+          width: MediaQuery.of(context).size.width * 100,
+          fit: BoxFit.cover,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 30, left: 10, right: 10),
+          decoration: BoxDecoration(
+              color: Color.fromARGB(50, 255, 255, 255),
+              borderRadius: BorderRadius.circular(10)),
+          child: TextFormField(
+            decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'لێگەریان',
+                prefixIcon: Icon(Icons.search),
+                prefixIconColor: Colors.white,
+                focusedBorder: InputBorder.none,
+                focusColor: Colors.white,
+                hintStyle:
+                    TextStyle(color: Colors.white, fontFamily: 'kurdish')),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:mshop/layout/appbar.dart';
-import 'package:mshop/layout/drawer.dart';
+import 'package:mshop/components/background_home.dart';
+// import 'package:mshop/layout/appbar.dart';
+// import 'package:mshop/layout/drawer.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar(),
-      drawer: drawer()
-    );
+    return Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+          // appBar: appBar(),
+          // drawer: drawer(),
+          body: ListView(
+            children: [
+              Column(
+                children: [BgHome()],
+              ),
+            ],
+          ),
+        ));
   }
 }
