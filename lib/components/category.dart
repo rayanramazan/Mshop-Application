6fc220fe0a2a7fd574a58a7d2f87be1663838e02
rayanramazan <rayanramazan09@gmail.com>
2023-02-04 16:41:47 +0300

    update

--- lib/components/category.dart
+++ lib/components/category.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mshop/anim/pageRoute.dart';
+import 'package:mshop/pages/product.dart';
 
-class Category extends StatelessWidget {
+class Category extends StatefulWidget {
   const Category({super.key});
 
+  @override
+  State<Category> createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,102 +33,112 @@ class Category extends StatelessWidget {
             ],
           ),
         ),
-        Container(
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 6),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(7, 0, 0, 0),
-                blurRadius: 4,
-                offset: Offset(2, 4), // Shadow position
-              ),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.female,
-                    size: 30,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "ئافرەتان",
-                    style: TextStyle(fontFamily: 'kurdish', fontSize: 18),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "١٢٧ بەردەستن",
-                    style:
-                        TextStyle(fontFamily: 'regular', color: Colors.black54),
-                  )
-                ],
-              ),
-              Icon(
-                Icons.arrow_right_outlined,
-                size: 30,
-              )
-            ],
+        InkWell(
+          onTap: () {
+            Navigator.push(context, pageRoute(widget: AllProduct()));
+          },
+          child: Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 6),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(7, 0, 0, 0),
+                  blurRadius: 4,
+                  offset: Offset(2, 4), // Shadow position
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.female,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "ئافرەتان",
+                      style: TextStyle(fontFamily: 'kurdish', fontSize: 18),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "١٢٧ بەردەستن",
+                      style: TextStyle(
+                          fontFamily: 'regular', color: Colors.black54),
+                    )
+                  ],
+                ),
+                Icon(
+                  Icons.arrow_right_outlined,
+                  size: 30,
+                )
+              ],
+            ),
           ),
         ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 6),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(7, 0, 0, 0),
-                blurRadius: 4,
-                offset: Offset(2, 4), // Shadow position
-              ),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.male,
-                    size: 30,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "زەڵامان",
-                    style: TextStyle(fontFamily: 'kurdish', fontSize: 18),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "٢٣٣ بەردەستن",
-                    style:
-                        TextStyle(fontFamily: 'regular', color: Colors.black54),
-                  )
-                ],
-              ),
-              Icon(
-                Icons.arrow_right_outlined,
-                size: 30,
-              )
-            ],
+        InkWell(
+          onTap: () {
+            Navigator.push(context, pageRoute(widget: AllProduct()));
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 6),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(7, 0, 0, 0),
+                  blurRadius: 4,
+                  offset: Offset(2, 4), // Shadow position
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.male,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "زەڵامان",
+                      style: TextStyle(fontFamily: 'kurdish', fontSize: 18),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "٢٣٣ بەردەستن",
+                      style: TextStyle(
+                          fontFamily: 'regular', color: Colors.black54),
+                    )
+                  ],
+                ),
+                Icon(
+                  Icons.arrow_right_outlined,
+                  size: 30,
+                )
+              ],
+            ),
           ),
         ),
       ],
